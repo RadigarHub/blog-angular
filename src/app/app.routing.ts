f64280ca9031bfaf9c9f6ff8f5d7c997cbbeaddd
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-20 16:03:52 +0200

    Clase 57

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+// IMPORTS NECESARIOS
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// IMPORTAR COMPONENTES
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+// DEFINIR LAS RUTAS
+const appRoutes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'inicio', component: LoginComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegisterComponent}
+];
+
+// EXPORTAR CONFIGURACIÓN
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
