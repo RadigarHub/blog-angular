39e64e7377142dcba384cfe55b3a20bb15da93eb
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-21 15:47:39 +0200

    Clase 58

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 // IMPORTAR COMPONENTES
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 // DEFINIR LAS RUTAS
 const appRoutes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'inicio', component: LoginComponent},
+    {path: '', component: HomeComponent},
+    {path: 'inicio', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegisterComponent}
+    {path: 'registro', component: RegisterComponent},
+    {path: '**', component: ErrorComponent} // Los ** significan cuando la ruta no exista y se debe de poner siempre al final de todas las rutas
 ];
 
 // EXPORTAR CONFIGURACIÓN
