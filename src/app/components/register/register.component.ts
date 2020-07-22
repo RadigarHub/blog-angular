e504456109ffd7ce4d8a9c62cae931f064b3e5ab
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-22 15:34:50 +0200

    Clase 61

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -8,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   public page_title: string;
+  public user: User;
 
   constructor() {
     this.page_title = 'Regístrate';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
 
   ngOnInit(): void {
     console.log('Componente de registro lanzado');
+  }
+
+  onSubmit(form) {
+    console.log(this.user);
   }
 
 }
