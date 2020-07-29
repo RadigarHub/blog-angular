e1eba653fdb729644cc91dbf908146514c10fa16
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-29 16:43:59 +0200

    Clase 110

--- src/app/components/category-detail/category-detail.component.ts
+++ src/app/components/category-detail/category-detail.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-category-detail',
   templateUrl: './category-detail.component.html',
-  styleUrls: ['./category-detail.component.css']
+  styleUrls: ['./category-detail.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryDetailComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public category: Category;
+  public posts: any;
+  public url: string;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _categoryService: CategoryService
+  ) {
+    this.url = global.url;
+  }
 
   ngOnInit(): void {
+    this.getPostsByCategory();
+  }
+
+  getPostsByCategory() {
+    this._route.params.subscribe(params => {
+      let id = Number(params['id']);
+
+      this._categoryService.getCategory(id).subscribe(
+        response => {
+          if (response.status == 'success') {
+            //console.log(response);
+            this.category = response.category;
+          } else {
+            this._router.navigate(['/inicio']);
+          }
+        },
+        error => {
+          console.log(<any>error);
+        }
+      )
+    });
   }
 
 }
