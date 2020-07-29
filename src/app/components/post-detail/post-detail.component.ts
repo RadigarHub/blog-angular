592013314f13ddc4e7028cb9b5ef628f8821146e
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-29 12:40:48 +0200

    Clase 100

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Post } from '../../models/post';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.css']
+  styleUrls: ['./post-detail.component.css'],
+  providers: [PostService]
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor() { }
+  public post: Post;
+
+  constructor(
+    private _postService: PostService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost() {
+    // Obtener el id del post de la url
+    this._route.params.subscribe(params => {
+      let id = Number(params['id']);
+      console.log(id);
+
+      // Petición ajax para obtener los datos del post
+      this._postService.getPost(id).subscribe(
+        response => {
+          if (response.status == 'success') {
+            this.post = response.post;
+            console.log(this.post);
+          } else {
+            this._router.navigate(['/inicio']);
+          }
+        },
+        error => {
+          console.log(<any>error);
+          this._router.navigate(['/inicio']);
+        }
+      );
+    });
   }
 
 }
