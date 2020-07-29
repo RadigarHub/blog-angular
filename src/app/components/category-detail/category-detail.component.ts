44c9b7c1f52772fe12314e705fe1e3873de6ff23
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-29 17:21:00 +0200

    Clase 112

--- src/app/components/category-detail/category-detail.component.ts
+++ src/app/components/category-detail/category-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { PostService } from '../../services/post.service';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
 import { global } from '../../services/global';
@@ -8,11 +10,13 @@ import { global } from '../../services/global';
   selector: 'app-category-detail',
   templateUrl: './category-detail.component.html',
   styleUrls: ['./category-detail.component.css'],
-  providers: [CategoryService]
+  providers: [UserService, PostService, CategoryService]
 })
 export class CategoryDetailComponent implements OnInit {
 
   public page_title: string;
+  public identity;
+  public token;
   public category: Category;
   public posts: any;
   public url: string;
@@ -20,8 +24,12 @@ export class CategoryDetailComponent implements OnInit {
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
+    private _userService: UserService,
+    private _postService: PostService,
     private _categoryService: CategoryService
   ) {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
     this.url = global.url;
   }
 
@@ -60,6 +68,17 @@ export class CategoryDetailComponent implements OnInit {
         }
       )
     });
+  }
+
+  deletePost(id) {
+    this._postService.delete(this.token, id).subscribe(
+      response => {
+        this.getPostsByCategory();
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
