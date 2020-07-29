4258d63b62b1770648761d3cfc0a9ef00f71ad68
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-29 11:04:29 +0200

    Clase 92

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Post} from '../models/post';
+import {global} from './global';
+
+@Injectable()
+export class PostService {
+    public url: string;
+    
+    constructor(
+        private _http: HttpClient
+    ) {
+        this.url = global.url;
+    }
+
+    pruebas() {
+        return "Hola desde el servicio de entradas";
+    }
+}
