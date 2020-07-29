5ba2100e8605d7813e4dc51f6e14f4a8f0f36730
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-29 09:22:38 +0200

    Clase 84

---
+++ src/app/components/post-new/post-new.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-new',
+  templateUrl: './post-new.component.html',
+  styleUrls: ['./post-new.component.css']
+})
+export class PostNewComponent implements OnInit {
+  
+  public page_title: string;
+
+  constructor() {
+    this.page_title = 'Crear nueva entrada';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
