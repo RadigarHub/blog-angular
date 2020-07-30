7d96a7c532ed4e5a35c7907d69f6ffef0f3f380c
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-30 12:24:07 +0200

    Clase 118

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+import { Post } from '../../models/post';
+import { User } from '../../models/user';
+import { PostService } from '../../services/post.service';
+import { UserService } from '../../services/user.service';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [PostService, UserService]
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public url: string;
+  public posts: Array<Post>;
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _postService: PostService,
+    private _userService: UserService
+  ) {
+    this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile() {
+    // Obtener el id del post de la url
+    this._route.params.subscribe(params => {
+      let userId = Number(params['id']);
+      this.getUser(userId);
+      this.getPosts(userId);
+    });
+  }
+
+  getUser(userId) {
+    this._userService.getUser(userId).subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.user = response.user;
+          console.log(this.user);
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  getPosts(userId) {
+    this._userService.getPosts(userId).subscribe(
+      response => {
+        if (response.status = 'success') {
+          this.posts = response.posts;
+          console.log(this.posts);
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  deletePost(id) {
+    this._postService.delete(this.token, id).subscribe(
+      response => {
+        this.getProfile();
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
